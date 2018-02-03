f47a2b41d3b8089b61b1ac05aebef1ebd8e1324e
Jose Herrera <jmhm2012@gmail.com>
2018-02-02 22:52:07 -0700

    All v1 (demo) features complete. Ready for QA
    todo:
    -explore token creation
    -webcam stream

--- server+frontend/public/hashFuncs/bundle.js
+++ server+frontend/public/hashFuncs/bundle.js
@@ -173,11 +173,10 @@ function ipfsHashCompose(hexHashPortion) {
     return bs58.encode(byteArr)
 }
 
-
-
-function ipfsHashDecompose() {
-
-
+function ipfsHashDecompose(ipfsHash) {
+    const bytes = bs58.decode(ipfsHash)
+    let _ipfsHashBytes=bytes.slice(2,34)
+    return "0x"+_ipfsHashBytes.toString("hex")
 }
 
 module.exports = {
