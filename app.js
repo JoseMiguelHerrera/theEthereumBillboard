7cf5b3363eaefd090a671f3a221b5aa979e4cb3b
Jose Herrera <jmhm2012@gmail.com>
2018-02-04 22:05:07 -0700

    fervid not showing up

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var ipfs = ipfsAPI('18.217.97.201', '5001', { protocol: 'http' })
 var app = express();
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
